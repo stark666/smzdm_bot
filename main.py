059ffe38ced61bd68e4bf6904e9d74451f962da5
stark666 <472217937@qq.com>
2020-12-12 10:15:25 +0800

    sc_key test v2

--- main.py
+++ main.py
@@ -57,9 +57,10 @@ if __name__ == '__main__':
     res = sb.checkin()
     print(res)
     SERVERCHAN_SECRETKEY = os.environ["SERVERCHAN_SECRETKEY"]
-    if SERVERCHAN_SECRETKEY is not None:
+    print('sc_key: ', SERVERCHAN_SECRETKEY)
+    if isinstance(SERVERCHAN_SECRETKEY,str) and len(SERVERCHAN_SECRETKEY)>0:
         print('检测到 SCKEY， 准备推送')
         push_to_wechat(text = '什么值得买每日签到',
-                        desp = str(res)',
+                        desp = str(res),
                         secretKey = SERVERCHAN_SECRETKEY)
     print('代码完毕')
