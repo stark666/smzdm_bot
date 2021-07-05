b45bfc4252aacae1b59269d283839876d965f4b3
hong <lun3322@126.com>
2021-07-05 11:21:06 +0800

    更换推送方式

--- utils/serverchan_push.py
+++ utils/serverchan_push.py
@@ -3,21 +3,31 @@ import config
 import requests
 
 
-def push_to_wechat(text,desp,secretKey):
+def push_to_wechat(text, desp, appToken, uid):
     """
-    通过serverchan将消息推送到微信
-    :param secretKey: severchan secretKey
-    :param text: 标题
-    :param desp: 内容
-    :return resp: json
+    通过wxpusher将消息推送到微信
     """
-    url = f'http://sctapi.ftqq.com/{secretKey}.send'
+    url = f'http://wxpusher.zjiecode.com/api/send/message'
     session = requests.Session()
-    data = {'text':text,'desp':desp}
-    resp = session.post(url,data = data)
+    data = {
+        "appToken": appToken,
+        "content": desp,
+        "summary": text,
+        "contentType": 1,
+        "topicIds": [],
+        "uids": [
+            uid
+        ],
+    }
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    resp = session.post(url, json=data, headers=headers)
     return resp.json()
 
 
 if __name__ == '__main__':
-    resp = push_to_wechat(text = 'test', desp='hi', secretKey= config.SERVERCHAN_SECRETKEY)
+    resp = push_to_wechat(text='test', desp='hi',
+                          appToken='',
+                          uid='')
     print(resp)
