63e313ff5eba08a54e9b4e06b6556d69c7173c8c
gzdxyq <94299483+gzdxyq@users.noreply.github.com>
2022-01-14 14:46:09 +0800

    Update config.py

--- config.py
+++ config.py
@@ -18,10 +18,9 @@ DEFAULT_HEADERS = {
 """
 调试用 COOKIE
 """
-TEST_COOKIE = '__ckguid=krH5pU8wnvSpGB4ilpqVFB4; device_id=21307064331637565539974629d6ce37645d986fedca23329b0bb88af5; homepage_sug=c; r_sort_type=score; __jsluid_s=32e4aa437ee3347a7043950590637d4c; __gads=ID=291a06df5c52ae1e:T=1637565542:S=ALNI_MbkuRxZ5_C1En4STU3rJV8YW3KK8g; sess=AT-z6Z43IQQ3WSeKuM/eMd2liglHar3eypkTeM/HHQreoq6+WVYd16gvo5WkBt26ujaIFuouCBnYQRmdHan4lSuzuac+unKp1X33ugQ5VRI0H5vyFRscQdgLfai; user=qq_q5iau|4169576740; smzdm_id=4169576740; smzdm_user_source=D0BD9CB05E05A3B9A48C649AACE75FFA; _zdmA.uid=ZDMA.d-D6IsLyQ.1641536239.2419200; _zdmA.vid=*; sensorsdata2015jssdkcross={"distinct_id":"4169576740","first_id":"17d46832b4b506-0b7347005cd4ed-978183a-1440000-17d46832b4c1ec","props":{"$latest_traffic_source_type":"自然搜索流量","$latest_search_keyword":"未取到值","$latest_referrer":"https://www.baidu.com/link","$latest_landing_page":"https://www.smzdm.com/"},"$device_id":"17d46832b4b506-0b7347005cd4ed-978183a-1440000-17d46832b4c1ec"}; Hm_lvt_9b7ac3d38f30fe89ff0b8a0546904e58=1640682711,1640761180,1640765788,1641536240; Hm_lpvt_9b7ac3d38f30fe89ff0b8a0546904e58=1641536240; footer_floating_layer=0; ad_date=7; bannerCounter=[{"number":0,"surplus":1},{"number":0,"surplus":1},{"number":0,"surplus":1},{"number":0,"surplus":1},{"number":0,"surplus":1},{"number":0,"surplus":1}]; ad_json_feed={}; amvid=307a38035b8d673ee5fed5c8e159fbc0; _zdmA.time=1641536306731.0.https://www.smzdm.com/'
-
+TEST_COOKIE = ''
 
 """
 调试用 SERVERCHAN_SECRETKEY
 """
-SERVERCHAN_SECRETKEY = 'SCT107857TCaWUQvmWCGLvL5NnwL9fiuuT'
+SERVERCHAN_SECRETKEY = ''
